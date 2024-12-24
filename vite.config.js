c9b08a715b33f30752439723882959c73f0fb404
Saquib Anjum <saquibanjum8969@gmail.com>
2024-12-24 19:03:13 +0530

    vercel-config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensures the app works with relative paths
-  build: {
-    outDir: 'dist' // Output directory for production build
-  },
-  server: {
-    historyApiFallback: true // Handles routes for development
-  }
-});
+})
